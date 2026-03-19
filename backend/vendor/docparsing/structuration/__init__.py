02c1e5e62632576240cf91bbc79f8d47b7fb43ed
Alex Argese <101403816+AlexArgese@users.noreply.github.com>
2026-03-20 00:13:36 +0100

    Delete backend/vendor/docparsing directory

--- backend/vendor/docparsing/structuration/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Document builder."""
-
-from .document_builder import DocumentBuilder
-from .utils import populate_paragraphs
-
-__all__ = [
-    "DocumentBuilder",
-    "populate_paragraphs",
-]
